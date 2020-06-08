c726acc8f1f26f0780fa6932c4bf759d2a1f6384
Nick Bensema <nickbensema@hotmail.com>
2020-06-07 20:15:35 -0700

    terminal displays feedback when listening for speech

--- speechchatter.py
+++ speechchatter.py
@@ -1,17 +1,23 @@
 
-from Recorder import record_audio, read_audio
 #from wit import Wit #6.0.0
 from chatters import Chatter
 import talkers
-import requests, json
+import requests, json, time, sys
 import speech_recognition as sr
-import time
 
-# TODO: use non-deprecated endpoint
 API_ENDPOINT = "https://api.wit.ai/speech"
 r = sr.Recognizer()
-r.pause_threshold = 0.1
+# TODO: make configurable in a more sensical way
+r.pause_threshold = 0.2
 r.non_speaking_duration = 0
+
+# utility method for printing progress notices and stuff
+def progress(*args,**kwargs):
+    if(sys.stdout.isatty()):
+        print("\033[2K\r", end='') # clear current line
+        print(*args,**kwargs)    # pass args to print()
+        print("\033[A", end='')  # UP
+        sys.stdout.flush()
 
 class SpeechChatter(Chatter):
     def __init__(self, auth_token, talker=talkers._instance):
@@ -24,18 +30,24 @@ class SpeechChatter(Chatter):
         #TODO: improve help to set up auth token
         self.auth_token = auth_token
         self.talker = talker
+        self.source = sr.Microphone()
 
     def run(self, bot):
         try:
             while True:
                 # record audio of specified length in specified audio file
-                with sr.Microphone() as source:
+                #with sr.Microphone() as source:
+                progress("listening", time.strftime("%H:%M:%S"))
+                with self.source as source:
                     audio = r.listen(source)
+                progress("converting")
                 wav_audio = audio.get_wav_data(
                             convert_rate=None if audio.sample_rate >= 8000 else 8000,  # audio samples must be at least 8 kHz
                             convert_width=2  # audio samples should be 16-bit
                         )
+                progress("recognizing via cloud")
                 result = self.recognize_bytes(wav_audio)
+                progress("")
                 try:
                     text = result['text']
                     print(">",text)
@@ -43,9 +55,11 @@ class SpeechChatter(Chatter):
                     print("*", response)
                     self.talker.say(response)
                 except KeyError:
-                    print("> ???", result.keys())
-                    if '_text' in result.keys():
-                        print(result["_text"])
+                    if 'entities' in result.keys() and result['entities'] == {}:
+                        print("? noise ", len(wav_audio))
+                        # TODO: figure out how to get actual duration
+                    else:
+                        print("# unknown response ", " ".join(result.keys()))
         except KeyboardInterrupt:
             pass # no op
 
@@ -59,7 +73,7 @@ class SpeechChatter(Chatter):
         resp = requests.post(API_ENDPOINT, headers = headers,
                                          data = audio)
         duration = time.time() - start
-        print(duration,"seconds to get response")
+        print("# %5.2f seconds to get response" % duration)
         # converting response content to JSON format
         data = json.loads(resp.content)
         return data
