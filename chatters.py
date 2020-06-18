92bc0ab81b0b8e33b007cb1bbd057fec8e59e6d3
Nick Bensema <nickbensema@hotmail.com>
2020-06-18 16:10:00 -0700

    refactored chatters to move more stuff to the superclass

--- chatters.py
+++ chatters.py
@@ -1,42 +1,45 @@
 #!/usr/bin/env python3
 
-pass
 class Chatter:
     """
     Abstract class for user interfaces to interface with chat.
-    """
-    pass
 
-class ScriptChatter(Chatter):
+    Subclasses must define __iter__() and say()
+    """
+    def run(self, bot):
+        if(hasattr(bot, "initial")):
+            self.say(bot.initial())
+        for line in self:
+            self.say(bot.respond(line))
+        if(hasattr(bot, "final")):
+            self.say(bot.final())
+
+class TextChatter(Chatter):
+    """
+    Reads input from the terminal, and writes responses to the terminal.
+    """
+    def __iter__(self):
+        try:
+            while True:
+                yield(input("> "))
+        except KeyboardInterrupt:
+            print("^C")
+        except EOFError:
+            pass
+
+    def say(self, response):
+        print("*", response)
+
+class ScriptChatter(TextChatter):
+    """
+    Reads input from a file, and writes responses to the terminal.
+    """
     def __init__(self, path):
         self.path = path
 
-    def run(self, bot):
-        if(hasattr(bot, "initial")):
-            print("*", bot.initial())
-        try:
-            with open(self.path) as f:
-                for line in f.readlines():
-                    print(">", line.rstrip())
-                    print("*", bot.respond(line.rstrip()))
-        except KeyboardInterrupt:
-            print("^C")
-            pass # no op, assume just broke out of loop
-        if(hasattr(bot, "final")):
-            print("*", bot.final())
+    def __iter__(self):
+        with open(self.path) as f:
+            for line in f.readlines():
+                print(">", line.rstrip())
+                yield(line.rstrip())
 
-
-class TextChatter(Chatter):
-    def run(self, bot):
-        if(hasattr(bot, "initial")):
-            print("*", bot.initial())
-        try:
-            while True:
-                line = input("> ") 
-                print("*", bot.respond(line))
-        except KeyboardInterrupt:
-            print("^C")
-            pass # no op, assume just broke out of loop
-        if(hasattr(bot, "final")):
-            print("*", bot.final())
-        
